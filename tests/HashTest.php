32a96af650964b0716bd90aa68715c722c40c87e
kos5454 <matteokos@orange.fr>
2026-03-18 10:32:20 +0100

    e

--- tests/HashTest.php
+++ tests/HashTest.php
@@ -4,7 +4,7 @@ use App\Utility\Hash;
 use PHPUnit\Framework\TestCase;
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
+//test pour la classe Hash pour vérifier que la méthode generate retourne bien une chaîne de caractères qui sert a hasher les mots de passe des utilisateurs
 class HashTest extends TestCase
 {
     public function testGenerateRetourneUneChaine() 
